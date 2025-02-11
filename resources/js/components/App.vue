5a9a8e2f042515c81b3d5f1760435838f968b2fc
Tučňáčik <martin.rakus1@gmail.com>
2025-02-11 15:39:08 +0100

    First UI version

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,3 +1,70 @@
 <template>
-    <h1 class="mb-4">Car Inventory</h1>
+    <div class="container mt-4">
+        <h1 class="mb-4">Car Inventory</h1>
+
+        <!-- Cars and Parts Lists -->
+        <div class="mt-5">
+            <h2>Available Cars</h2>
+            <ul class="list-group mb-4">
+                <li v-for="car in cars" :key="car.id" class="list-group-item d-flex justify-content-between align-items-center">
+                <span>
+                    {{ car.name }}
+                    <span v-if="car.registration_number && car.is_registered"> - {{ car.registration_number }}</span>
+                    -
+                    <span v-if="car.is_registered" class="badge bg-success">Registered</span>
+                    <span v-else class="badge bg-danger">Not Registered</span>
+                </span>
+                </li>
+            </ul>
+
+            <h2>Available Parts</h2>
+            <ul class="list-group">
+                <li v-for="part in parts" :key="part.id" class="list-group-item d-flex justify-content-between align-items-center">
+                    <span>{{ part.name }}</span>
+                    <span class="badge bg-primary">${{ part.price }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                car: {
+                    name: '',
+                    registration_number: '',
+                    is_registered: false,
+                },
+                cars: [],
+                parts: [],
+            };
+        },
+        methods: {
+            fetchCars() {
+                axios.get('/cars')
+                .then(response => {
+                    this.cars = response.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            },
+            fetchParts() {
+                axios.get('/parts')
+                .then(response => {
+                    this.parts = response.data;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            }
+        },
+        mounted() {
+            this.fetchCars();
+            this.fetchParts();
+        }
+    };
+</script>
+
