3c180c9de03d2cac28b57601eb143ff8fa8c46b9
Tučňáčik <martin.rakus1@gmail.com>
2025-02-11 14:57:08 +0100

    Updated dependencies and config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,23 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
     ],
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+
+    server: {
+        proxy: {
+            '/app': 'http://127.0.0.1',
+        },
+    },
+
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+    },
 });
